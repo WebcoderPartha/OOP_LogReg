3330f6e447e6d4c6e13473fda03ca5a3da74cf7e
Parthadeb Mondal <parthadev76@gmail.com>
2020-10-02 14:51:15 +0600

    template customization

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+
+    <title>Login Register System With OOP</title>
+</head>
+<body>
+
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Brand</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="profile.php">Profile</a></li>
+                        <li><a href="l">Logout</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>User List <span class="pull-right"><strong>Welcome!</strong> Parthad</span></h4>
+            </div>
+            <div class="panel-body">
+                 <table class="table table-hover">
+                  <thead>
+                     <tr>
+                         <th>Serial</th>
+                         <th>Name</th>
+                         <th>Username</th>
+                         <th>Email</th>
+                         <th>Action</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+                         <td>1</td>
+                         <td>Partha</td>
+                         <td>partha87</td>
+                         <td>partha@gmail.com</td>
+                         <td>
+                             <a href="">Edit</a>
+                             <a href="">Delete</a>
+                         </td>
+                     </tr>
+                     <tr>
+                         <td>1</td>
+                         <td>Partha</td>
+                         <td>partha87</td>
+                         <td>partha@gmail.com</td>
+                         <td>
+                             <a href="">Edit</a>
+                             <a href="">Delete</a>
+                         </td>
+                     </tr>
+                     <tr>
+                         <td>1</td>
+                         <td>Partha</td>
+                         <td>partha87</td>
+                         <td>partha@gmail.com</td>
+                         <td>
+                             <a href="">Edit</a>
+                             <a href="">Delete</a>
+                         </td>
+                     </tr>
+                     <tr>
+                         <td>1</td>
+                         <td>Partha</td>
+                         <td>partha87</td>
+                         <td>partha@gmail.com</td>
+                         <td>
+                             <a href="">Edit</a>
+                             <a href="">Delete</a>
+                         </td>
+                     </tr>
+                  </tbody>
+                  </table>
+            </div>
+        </div>
+        <div class="well">www.facebook.com</div>
+
+    </div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
